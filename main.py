f847af3b995f46d1f6b6d233f8b34fe0a092d4ea
1799aperpetua <aperpetua1799@gmail.com>
2023-04-11 23:23:26 -0400

    This is a bit of a mess right now but it's because I tried blending back into CustomTKinter because i prefer the look is significantly more professional and if I'm handing this to my boss, I'd like it to look nice.  Going to make a quick overhaul into customtkinter, but will be easy since I have the framework and I can just transfer the key methods and operations

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import tkinter as tk
+import customtkinter as ctk
 from tkinter import filedialog
 import os
 
-class App(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
+class App(ctk.CTk):
+    def __init__(self):
+        
+        #self.geometry("500x500")
+        #self.title("Scorecard Compiler")
+        #self.minsize(900, 400)
+        
+        self.frame = ctk.CTkFrame(master = self).pack(padx = 5, pady = 5)
 
-        self.current_master_file = ''
-        self.current_chosen_dir = ''
+        #super().__init__(master)
+        #self.master = master
+        #self.create_widgets()
+
+        self.current_master_file = None
+        self.current_chosen_dir = None
+
+        # Select operating system radiobutton
+        self.os_label = ctk.Label(self, text = 'Select your operating system:').pack(side = 'top', padx = 10, pady = (20, 8))
+        self.os_var = tk.StringVar(value = 'Windows')
+        self.os_button1 = tk.Radiobutton(self, text = 'Windows', variable = self.os_var, value = 'Windows').pack(side = 'top', padx = 10 , pady = (0, 4))
+        self.os_button2 = tk.Radiobutton(self, text = 'Mac', variable = self.os_var, value = 'Mac').pack(side = 'top', padx = 10 , pady = (0, 12))
+
+        # Master file input
+        self.master_file_label = tk.Label(self, text = 'Which file would you like to add a package of scorecard sheets to?').pack(padx = 10, pady = (10, 4))
+        tk.Button(self, text = "Select Master File", command = self.open_file).pack(padx = 10, pady = (0, 10))
+
+        self.select_directory_label = tk.Label(self, text = 'What folder are your scorecards located in?').pack( padx = 10, pady = (10, 4))
+        tk.Button(self, text = "Select Package", command = self.open_directory).pack(padx = 10, pady = (0, 20))
+
+        
 
     def open_directory(self):
         dir = filedialog.askdirectory()
@@ -28,30 +50,26 @@ class App(tk.Frame):
             tk.Label(self, text = 'The file selected is located at: ' + str(file.name)).pack(padx = 10, pady = 5)
             
 
-    def create_widgets(self):
+    #def create_widgets(self):
 
-        # self.geometry('1000x500')
-        # self.title("Scorecard Compiler")
-        # self.minsize(500, 400)
+        # # Select operating system radiobutton
+        # self.os_label = ctk.Label(self, text = 'Select your operating system:').pack(side = 'top', padx = 10, pady = (20, 8))
+        # self.os_var = tk.StringVar(value = 'Windows')
+        # self.os_button1 = tk.Radiobutton(self, text = 'Windows', variable = self.os_var, value = 'Windows').pack(side = 'top', padx = 10 , pady = (0, 4))
+        # self.os_button2 = tk.Radiobutton(self, text = 'Mac', variable = self.os_var, value = 'Mac').pack(side = 'top', padx = 10 , pady = (0, 12))
 
-        # Select operating system radiobutton
-        self.os_label = tk.Label(self, text = 'Select your operating system:').pack(side = 'top', padx = 10, pady = (20, 8))
-        self.os_var = tk.StringVar(value = 'Windows')
-        self.os_button1 = tk.Radiobutton(self, text = 'Windows', variable = self.os_var, value = 'Windows').pack(side = 'top', padx = 10 , pady = (0, 4))
-        self.os_button2 = tk.Radiobutton(self, text = 'Mac', variable = self.os_var, value = 'Mac').pack(side = 'top', padx = 10 , pady = (0, 12))
+        # # Master file input
+        # self.master_file_label = tk.Label(self, text = 'Which file would you like to add a package of scorecard sheets to?').pack(padx = 10, pady = (10, 4))
+        # tk.Button(self, text = "Select Master File", command = self.open_file).pack(padx = 10, pady = (0, 10))
 
-        # Master file input
-        self.master_file_label = tk.Label(self, text = 'Which file would you like to add a package of scorecard sheets to?').pack(padx = 10, pady = (10, 4))
-        tk.Button(self, text = "Select Master File", command = self.open_file).pack(padx = 10, pady = (0, 10))
-
-        self.select_directory_label = tk.Label(self, text = 'What folder are your scorecards located in?').pack( padx = 10, pady = (10, 4))
-        tk.Button(self, text = "Select Package", command = self.open_directory).pack(padx = 10, pady = (0, 20))
+        # self.select_directory_label = tk.Label(self, text = 'What folder are your scorecards located in?').pack( padx = 10, pady = (10, 4))
+        # tk.Button(self, text = "Select Package", command = self.open_directory).pack(padx = 10, pady = (0, 20))
         
 
 
 
         
 if __name__ == '__main__':
-    root = tk.Tk()
-    app = App(master = root)
+    #root = ctk.CTk()
+    app = App()
     app.mainloop()
